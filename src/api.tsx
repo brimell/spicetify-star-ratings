982496bc60ec630e6880534327d5dbb6f1912e9f
Raaydon <billjrimell@gmail.com>
2024-02-16 13:36:51 +0000

    clean up code

--- src/api.tsx
+++ src/api.tsx
@@ -46,20 +46,18 @@ export async function getContents() {
     return await Spicetify.Platform.RootlistAPI.getContents();
 }
 
-export async function addTrackToPlaylist(playlistUri: string, trackUri: string) {
-    await Spicetify.Platform.PlaylistAPI.add(playlistUri, [trackUri], {after: 1, before: 0});
-}
-
 export async function addTrackToLikedSongs(trackUri: string) {
     const trackId = trackUri.replace("spotify:track:", "");
     // console.log('this',await Spicetify.Platform.LibraryAPI.add())
     await Spicetify.CosmosAsync.put(`https://api.spotify.com/v1/me/tracks?ids=${trackId}`);
 }
 
+export async function addTrackToPlaylist(playlistUri: string, trackUri: string) {
+    await Spicetify.Platform.PlaylistAPI.add(playlistUri, [trackUri], {after: 1, before: 0});
+}
+
 export async function removeTrackFromPlaylist(playlistUri: string, trackUri: string) {
-    console.log(Spicetify.Platform);
-    const res = await Spicetify.Platform.PlaylistAPI.remove(playlistUri, [{uri: trackUri, uid: ""}]);
-    console.log(res);
+    await Spicetify.Platform.PlaylistAPI.remove(playlistUri, [{uri: trackUri, uid: ""}]);
 }
 
 export async function getPlaylistItems(uri: string) {
@@ -74,7 +72,7 @@ export async function isAppLaterThan(specifiedVersion: string) {
     return result === 1;
 }
 
-export async function moveTracksBefore(playlistUri, trackUids, beforeUid) {
+export async function moveTracksBefore(playlistUri: string, trackUids, beforeUid: string) {
     const isV2 = await isAppLaterThan("1.2.5.1006.g22820f93");
     await Spicetify.Platform.PlaylistAPI.move(
         playlistUri,
@@ -83,7 +81,7 @@ export async function moveTracksBefore(playlistUri, trackUids, beforeUid) {
     );
 }
 
-export async function moveTracksAfter(playlistUri, trackUids, afterUid) {
+export async function moveTracksAfter(playlistUri: string, trackUids, afterUid: string) {
     const isV2 = await isAppLaterThan("1.2.5.1006.g22820f93");
     await Spicetify.Platform.PlaylistAPI.move(
         playlistUri,
